5b1f4a6f1192997df8b8dd6e7b0eee7811774211
Ouail Laamiri <laamiri.ouail@etu.uae.ac.ma>
2023-12-27 09:50:16 +0100

    test

--- app/middlewares/verifySignUp.js
+++ app/middlewares/verifySignUp.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcryptjs");
 const User = db.user;
 
 const checkDuplicateEmailOrCin = (req, res, next) => {
-  const { email, cin } = req.body;
+  const { email, nationalId } = req.body;
 
   // Check for duplicate email
   User.findOne({ email }).exec((emailErr, emailUser) => {
@@ -14,7 +14,7 @@ const checkDuplicateEmailOrCin = (req, res, next) => {
     if (emailUser) {
       return res.status(400).send({token:null, message: "Failed! Email is already in use!" });
     }
-    const cin=bcrypt.hashSync(req.body.nationalId, 8);
+    const cin=bcrypt.hashSync(nationalId, 8);
     // Check for duplicate Cin
     User.findOne({ cin }).exec((cinErr, cinUser) => {
       if (cinErr) {
